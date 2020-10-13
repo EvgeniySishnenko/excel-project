7d1109a6d8cddd0c548c4686c38fc23959aeff24
“Евгений <sishnenko86@mail.ru>
2020-10-13 22:16:51 +0300

    com

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const loader = require("sass-loader");
+
 const isProd = process.env.NODE_ENV === "production";
 const isDev = !isProd;
+
 const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+
 const jsLoaders = () => {
   const loaders = [
     {
       loader: "babel-loader",
       options: {
         presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-proposal-class-properties"],
       },
     },
   ];
+
   if (isDev) {
-    // loaders.push("eslint-loader");
+    loaders.push("eslint-loader");
   }
+
   return loaders;
 };
 
@@ -39,12 +44,13 @@ module.exports = {
   },
   devtool: isDev ? "source-map" : false,
   devServer: {
+    index: "index.html",
     port: 3000,
-    hot: isDev,
+    // hot: isDev,
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
+    new HTMLWebpackPlugin({
       template: "index.html",
       minify: {
         removeComments: isProd,
@@ -81,7 +87,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.m?js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: jsLoaders(),
       },
